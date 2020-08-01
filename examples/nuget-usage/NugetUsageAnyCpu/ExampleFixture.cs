2933bc80389006578ca6975c8f4c7391bd5c6d96
Modest-as <modestas.petravicius@gmail.com>
2020-08-02 00:44:46 +0300

    new nuget examples

---
+++ examples/nuget-usage/NugetUsageAnyCpu/ExampleFixture.cs
@@ -0,0 +1,24 @@
+﻿using Docnet.Core;
+using System;
+using Xunit;
+
+namespace NugetUsageAnyCpu
+{
+    public class ExampleFixture : IDisposable
+    {
+        public IDocLib DocNet { get; }
+
+        public ExampleFixture()
+        {
+            DocNet = DocLib.Instance;
+        }
+
+        public void Dispose()
+        {
+            DocNet.Dispose();
+        }
+    }
+
+    [CollectionDefinition("Example collection")]
+    public class ExampleCollection : ICollectionFixture<ExampleFixture> { }
+}
