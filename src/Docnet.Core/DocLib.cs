d9ffc97c473c1df4334cbba4bbed935686050fe8
Jess Thrysoee <jess@thrysoee.dk>
2020-09-27 15:35:52 +0200

    Capture LoadDocument error code with DocnetLoadDocumentException

--- src/Docnet.Core/DocLib.cs
+++ src/Docnet.Core/DocLib.cs
@@ -4,6 +4,7 @@ using Docnet.Core.Editors;
 using Docnet.Core.Models;
 using Docnet.Core.Readers;
 using Docnet.Core.Validation;
+using Docnet.Core.Exceptions;
 
 // ReSharper disable ParameterOnlyUsedForPreconditionCheck.Local
 namespace Docnet.Core
@@ -165,28 +166,7 @@ namespace Docnet.Core
             lock (Lock)
             {
                 var code = fpdf_view.FPDF_GetLastError();
-
-                switch (code)
-                {
-                    case 0:
-                        return "no error";
-                    case 1:
-                        return "unknown error";
-                    case 2:
-                        return "file not found or could not be opened";
-                    case 3:
-                        return "file not in PDF format or corrupted";
-                    case 4:
-                        return "password required or incorrect password";
-                    case 5:
-                        return "unsupported security scheme";
-                    case 6:
-                        return "page not found or content error";
-                    case 1001:
-                        return "the requested operation cannot be completed due to a license restrictions";
-                    default:
-                        return "unknown error";
-                }
+                return LastError.ErrorCodePhrase(code);
             }
         }
 
