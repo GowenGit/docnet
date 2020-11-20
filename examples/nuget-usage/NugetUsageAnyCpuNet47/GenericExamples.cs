5eaca33c7362d57cc7fd4dda0d1d9adf5ca7dad5
Modest-as <modestas.petravicius@gmail.com>
2020-11-20 16:18:47 +0200

    net47 support

---
+++ examples/nuget-usage/NugetUsageAnyCpuNet47/GenericExamples.cs
@@ -0,0 +1,30 @@
+﻿using Docnet.Core.Models;
+using Xunit;
+
+namespace NugetUsageAnyCpuNet47
+{
+    [Collection("Example collection")]
+    public class GenericExamples
+    {
+        private const string Path = "Assets/wikipedia_0.pdf";
+
+        private readonly ExampleFixture _fixture;
+
+        public GenericExamples(ExampleFixture fixture)
+        {
+            _fixture = fixture;
+        }
+
+        [Fact]
+        public void GetPageCount_WhenCalled_ShouldSucceed()
+        {
+            using (var docReader = _fixture.DocNet.GetDocReader(
+                Path,
+                new PageDimensions(1080, 1920)))
+            {
+                Assert.Equal(20, docReader.GetPageCount());
+
+            }
+        }
+    }
+}
---
+++ examples/nuget-usage/NugetUsageAnyCpuNet47/PdfToImageExamples.cs
@@ -0,0 +1,79 @@
+﻿using Docnet.Core.Models;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Drawing.Imaging;
+using System.IO;
+using System.Runtime.InteropServices;
+using Xunit;
+
+namespace NugetUsageAnyCpuNet47
+{
+    [Collection("Example collection")]
+    public class PdfToImageExamples
+    {
+        private const string Path = "Assets/wikipedia_0.pdf";
+
+        private readonly ExampleFixture _fixture;
+
+        public PdfToImageExamples(ExampleFixture fixture)
+        {
+            _fixture = fixture;
+        }
+
+        [Fact]
+        public void ConvertPageToSimpleImageWithLetterOutlines_WhenCalled_ShouldSucceed()
+        {
+            using (var docReader = _fixture.DocNet.GetDocReader(
+                Path,
+                new PageDimensions(1080, 1920)))
+            using (var pageReader = docReader.GetPageReader(0))
+            {
+                var rawBytes = pageReader.GetImage();
+
+                var width = pageReader.GetPageWidth();
+                var height = pageReader.GetPageHeight();
+
+                var characters = pageReader.GetCharacters();
+
+                using (var bmp = new Bitmap(width, height, PixelFormat.Format32bppArgb))
+                {
+                    AddBytes(bmp, rawBytes);
+                    DrawRectangles(bmp, characters);
+
+                    using (var stream = new MemoryStream())
+                    {
+
+                        bmp.Save(stream, ImageFormat.Png);
+
+                        File.WriteAllBytes("../../../output_image.png", stream.ToArray());
+                    }
+                }
+            }
+        }
+
+        private static void AddBytes(Bitmap bmp, byte[] rawBytes)
+        {
+            var rect = new Rectangle(0, 0, bmp.Width, bmp.Height);
+
+            var bmpData = bmp.LockBits(rect, ImageLockMode.WriteOnly, bmp.PixelFormat);
+            var pNative = bmpData.Scan0;
+
+            Marshal.Copy(rawBytes, 0, pNative, rawBytes.Length);
+            bmp.UnlockBits(bmpData);
+        }
+
+        private static void DrawRectangles(Bitmap bmp, IEnumerable<Character> characters)
+        {
+            var pen = new Pen(Color.Red);
+
+            using (var graphics = Graphics.FromImage(bmp))
+            {
+                foreach (var c in characters)
+                {
+                    var rect = new Rectangle(c.Box.Left, c.Box.Top, c.Box.Right - c.Box.Left, c.Box.Bottom - c.Box.Top);
+                    graphics.DrawRectangle(pen, rect);
+                }
+            }
+        }
+    }
+}
