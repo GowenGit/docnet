270d7a850e7c625ea757895000623f7f8c9cefa1
Nikolai Martens Låstad <nikolai.martens.lastad@kdi.kongsberg.com>
2020-11-24 15:01:09 +0100

    remove a unit test

--- src/Docnet.Tests.Unit/Models/BoundBoxTests.cs
+++ src/Docnet.Tests.Unit/Models/BoundBoxTests.cs
@@ -6,17 +6,20 @@ namespace Docnet.Tests.Unit.Models
 {
     public class BoundBoxTests
     {
-        [Theory]
-        [InlineData(-1, 0, 0, 0)]
-        [InlineData(0, -1, 0, 0)]
-        [InlineData(0, 0, -1, 0)]
-        [InlineData(0, 0, 0, -1)]
-        [InlineData(5, 0, 2, 0)]
-        [InlineData(0, 5, 0, 2)]
-        public void Ctor_WhenCalledWithInvalidVersion_ShouldThrow(int left, int top, int right, int bottom)
+        // test removed to allow bounding boxes with origin bottom left
+        /*
+        [theory]
+        [inlinedata(-1, 0, 0, 0)]
+        [inlinedata(0, -1, 0, 0)]
+        [inlinedata(0, 0, -1, 0)]
+        [inlinedata(0, 0, 0, -1)]
+        [inlinedata(5, 0, 2, 0)]
+        [inlinedata(0, 5, 0, 2)]
+        public void ctor_whencalledwithinvalidversion_shouldthrow(int left, int top, int right, int bottom)
         {
-            Assert.Throws<ArgumentException>(() => new BoundBox(left, top, right, bottom));
+            assert.throws<argumentexception>(() => new boundbox(left, top, right, bottom));
         }
+        */
 
         [Theory]
         [InlineData(0, 0, 0, 0)]
