b98eb4056157323eef55d615c46ab2ab51b09f52
Christoffer Green <christoffer.green@gmail.com>
2021-12-05 16:32:32 +0100

    Add Arm/64-bit target binary (#53)
    Grab Arm/64-bit target pdfium.so from pdfium-binaries repo.

--- tools/get_pdfium.sh
+++ tools/get_pdfium.sh
@@ -2,24 +2,28 @@
 
 wget https://github.com/bblanchon/pdfium-binaries/releases/download/chromium%2F4655/pdfium-linux-x64.tgz
 wget https://github.com/bblanchon/pdfium-binaries/releases/download/chromium%2F4655/pdfium-linux-arm.tgz
+wget https://github.com/bblanchon/pdfium-binaries/releases/download/chromium%2F4722/pdfium-linux-arm64.tgz
 wget https://github.com/bblanchon/pdfium-binaries/releases/download/chromium%2F4655/pdfium-windows-x64.tgz
 wget https://github.com/bblanchon/pdfium-binaries/releases/download/chromium%2F4655/pdfium-windows-x86.tgz
 wget https://github.com/bblanchon/pdfium-binaries/releases/download/chromium%2F4655/pdfium-darwin-x64.tgz
 
 mkdir linux
 mkdir linux-arm
+mkdir linux-arm64
 mkdir windows
 mkdir windowsx86
 mkdir osx
 
 tar -xvf pdfium-linux-x64.tgz -C linux
 tar -xvf pdfium-linux-arm.tgz -C linux-arm
+tar -xvf pdfium-linux-arm64.tgz -C linux-arm64
 tar -xvf pdfium-darwin-x64.tgz -C osx
 tar -xvf pdfium-windows-x64.tgz -C windows
 tar -xvf pdfium-windows-x86.tgz -C windowsx86
 
 mkdir -p ../src/Docnet.Core/runtimes/linux/native/
 mkdir -p ../src/Docnet.Core/runtimes/linux-arm/native/
+mkdir -p ../src/Docnet.Core/runtimes/linux-arm64/native/
 mkdir -p ../src/Docnet.Core/runtimes/osx/native/
 mkdir -p ../src/Docnet.Core/runtimes/win-x64/native/
 mkdir -p ../src/Docnet.Core/runtimes/win-x86/native/
@@ -30,6 +34,9 @@ cp linux/LICENSE ../src/Docnet.Core/runtimes/linux/native/LICENSE
 cp linux-arm/lib/libpdfium.so ../src/Docnet.Core/runtimes/linux-arm/native/pdfium.so
 cp linux-arm/LICENSE ../src/Docnet.Core/runtimes/linux-arm/native/LICENSE
 
+cp linux-arm64/lib/libpdfium.so ../src/Docnet.Core/runtimes/linux-arm64/native/pdfium.so
+cp linux-arm64/LICENSE ../src/Docnet.Core/runtimes/linux-arm64/native/LICENSE
+
 cp osx/lib/libpdfium.dylib ../src/Docnet.Core/runtimes/osx/native/pdfium.dylib
 cp osx/LICENSE ../src/Docnet.Core/runtimes/osx/native/LICENSE
 
@@ -39,5 +46,5 @@ cp windows/LICENSE ../src/Docnet.Core/runtimes/win-x64/native/LICENSE
 cp windowsx86/x86/bin/pdfium.dll ../src/Docnet.Core/runtimes/win-x86/native/pdfium.dll
 cp windowsx86/LICENSE ../src/Docnet.Core/runtimes/win-x86/native/LICENSE
 
-rm pdfium-linux-x64.tgz pdfium-linux-arm.tgz pdfium-windows-x64.tgz pdfium-windows-x86.tgz pdfium-darwin-x64.tgz
-rm -rf linux linux-arm windows windowsx86 osx
+rm pdfium-linux-x64.tgz pdfium-linux-arm.tgz pdfium-linux-arm64.tgz pdfium-windows-x64.tgz pdfium-windows-x86.tgz pdfium-darwin-x64.tgz
+rm -rf linux linux-arm linux-arm64 windows windowsx86 osx
